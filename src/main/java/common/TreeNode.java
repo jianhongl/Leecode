269ec8695cd5fcf2fc8bc07750965c8c211ffda0
skyfall2016 <805747062@qq.com>
2021-12-14 22:58:57 +0800

    增加公共方法

---
+++ src/main/java/common/TreeNode.java
@@ -0,0 +1,17 @@
+/**
+ * FileName: TreeNode
+ * Author:   Jianhongl
+ * Date:     14/12/21 10:57 pm
+ * Description:
+ * History:
+ */
+package common;
+
+/**
+ * @author lujianhong
+ * @create 14/12/21
+ * @since 1.0.0
+ */
+public class TreeNode {
+
+}
