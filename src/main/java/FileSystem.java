b02002d8cae5facdfb44e56cc112edadd2c82c72
skyfall2016 <805747062@qq.com>
2021-12-15 23:19:18 +0800

    文件系统 Leetcode1166

---
+++ src/main/java/FileSystem.java
@@ -0,0 +1,44 @@
+/**
+ * FileName: FileSystem
+ * Author:   Jianhongl
+ * Date:     15/12/21 11:18 pm
+ * Description:
+ * History:
+ */
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author lujianhong
+ * @create 15/12/21
+ * @since 1.0.0
+ */
+public class FileSystem {
+    private Map<String, Integer> files;
+
+    public FileSystem() {
+        files = new HashMap<>();
+
+        files.put("", -1);
+    }
+
+    public boolean createPath(String path, int value) {
+        if (files.containsKey(path)) {
+            return false;
+        }
+
+        int last = path.lastIndexOf("/");
+        String prevRoot = path.substring(0, last);
+        if (!files.containsKey(prevRoot)) {
+            return false;
+        }
+
+        files.put(path, value);
+        return true;
+    }
+
+    public int get(String path) {
+        return files.getOrDefault(path, -1);
+    }
+}
